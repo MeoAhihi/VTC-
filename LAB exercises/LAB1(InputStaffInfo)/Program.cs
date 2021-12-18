8d4629dfec6e1553ed9ad3c12b73faf0fe7a683c
MeoAhihi <viphongly2804@gmail.com>
2021-12-18 07:53:09 +0700

    LAB 1 done

---
+++ LAB exercises/LAB1(InputStaffInfo)/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("***Candidate 1:");
+Console.Write("\t+ Full name: "); string fullName1 = Console.ReadLine();
+Console.Write("\t+ Birth year: "); short birthYear1 = Convert.ToInt16(Console.ReadLine());
+Console.Write("\t+ Gender: "); char gender1 = char.Parse(Console.ReadLine());
+Console.Write("\t+ Height: "); float height1 = float.Parse(Console.ReadLine());
+Console.Write("\t+ Weight: "); byte weight1 = Convert.ToByte(Console.ReadLine());
+Console.Write("\t+ Home town: "); string homeTown1 = Console.ReadLine();
+Console.Write("\t+ Major: "); string major1 = Console.ReadLine();
+Console.Write("\t+ Averange Score: "); float avgScore1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("***Candidate 2:");
+Console.Write("\t+ Full name: "); string fullName2 = Console.ReadLine();
+Console.Write("\t+ Birth year: "); short birthYear2 = Convert.ToInt16(Console.ReadLine());
+Console.Write("\t+ Gender: "); char gender2 = char.Parse(Console.ReadLine());
+Console.Write("\t+ Height: "); float height12= float.Parse(Console.ReadLine());
+Console.Write("\t+ Weight: "); byte weight2 = Convert.ToByte(Console.ReadLine());
+Console.Write("\t+ Home town: "); string homeTown2 = Console.ReadLine();
+Console.Write("\t+ Major: "); string major2 = Console.ReadLine();
+Console.Write("\t+ Averange Score: "); float avgScore2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("***Candidate 3:");
+Console.Write("\t+ Full name: "); string fullName3 = Console.ReadLine();
+Console.Write("\t+ Birth year: "); short birthYear3 = Convert.ToInt16(Console.ReadLine());
+Console.Write("\t+ Gender: "); char gender3 = char.Parse(Console.ReadLine());
+Console.Write("\t+ Height: "); float height3 = float.Parse(Console.ReadLine());
+Console.Write("\t+ Weight: "); byte weight3 = Convert.ToByte(Console.ReadLine());
+Console.Write("\t+ Home town: "); string homeTown3 = Console.ReadLine();
+Console.Write("\t+ Major: "); string major3 = Console.ReadLine();
+Console.Write("\t+ Averange Score: "); float avgScore3 = float.Parse(Console.ReadLine());
