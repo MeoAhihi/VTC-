bcf8f5e617d0dff64018b71e2d055161714091bc
MeoAhihi <viphongly2804@gmail.com>
2021-12-18 08:02:26 +0700

    function for Prime and Perfect number test

---
+++ 1000BT/PerfectNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿bool PerfectTest(uint number)
+{
+    uint sum = 0;
+    for (uint i = 1; i < number; i++) if (number % i == 0) sum += i;
+
+    if (number == sum) return true;
+    else return false;
+}
+
+bool PrimeTest(uint number) {
+    
+    bool isPrime = true;
+    for (uint i = 2; i < number; i++) {
+        if (number % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    return isPrime;
+}
+
